962daa55cf97f96c9aa6e06e9e0fffd81a3fef2b
Rayen <rayenebenrehouma@outlook.fr>
2023-01-02 13:33:57 +0100

    finalisation soutenance

--- app/repository/UserRepository.php
+++ app/repository/UserRepository.php
@@ -13,49 +13,49 @@ class UserRepository
      * @return void
      */
     public function addUser($user){
-                $BDD = new Database();
-                $DB= $BDD->connect();
-                $sql = $DB->prepare("INSERT INTO `utilisateurs`(`identifiant`, `email`, `password`, `role`,`status`) VALUES (?,?,?,'0','0')");
-                $result = $sql->execute(array($user->getIdentifiant(),$user->getEmail(),$user->getPassword()));
-                if($result){
-                    echo 'Votre compte à bien été ajouter';
-                }else{
-                    echo 'bug non trouvé';
-                }
-            }
+        $BDD = new Database();
+        $DB= $BDD->connect();
+        $sql = $DB->prepare("INSERT INTO `utilisateurs`(`identifiant`, `email`, `password`, `role`,`status`) VALUES (?,?,?,'0','0')");
+        $result = $sql->execute(array($user->getIdentifiant(),$user->getEmail(),$user->getPassword()));
+        if($result){
+            echo 'Votre compte à bien été ajouter';
+        }else{
+            echo 'bug non trouvé';
+        }
+    }
 
     /**
      * @param $user
      * @return void
      */
     public function connectUser($user){
-            $BDD = new Database();
-            $DB= $BDD->connect();
-            $check = $DB->prepare('SELECT id_utilisateur, identifiant, email, password, role FROM utilisateurs WHERE email = ? AND status = 1');
-            $result = $check->execute(array($user->getEmail()));
+        $BDD = new Database();
+        $DB= $BDD->connect();
+        $check = $DB->prepare('SELECT id_utilisateur, identifiant, email, password, role FROM utilisateurs WHERE email = ? AND status = 1');
+        $result = $check->execute(array($user->getEmail()));
 
-            $data = $check->fetch();
-            $row = $check->rowCount();
-            $passwordChek = password_verify($user->getPassword(),$data["password"]);
+        $data = $check->fetch();
+        $row = $check->rowCount();
+        $passwordChek = password_verify($user->getPassword(),$data["password"]);
 
 
-            if($row == 1) {
-                if ($passwordChek == true) {
-                    $datas = new User();
-                    $datas->setId($data["id_utilisateur"]);
-                    $datas->setIdentifiant($data["identifiant"]);
-                    $datas->setEmail($data["email"]);
-                    $datas->setPassword($data['password']);
-                    $datas->setRole($data["role"]);
-                    //changer vraiment
-                    session_start();
-                    $_SESSION['user'] = $datas;
-                    header('location: /accueil');
-                }
-            }
+        if($row == 1) {
+            if ($passwordChek == true) {
+                $datas = new User();
+                $datas->setId($data["id_utilisateur"]);
+                $datas->setIdentifiant($data["identifiant"]);
+                $datas->setEmail($data["email"]);
+                $datas->setPassword($data['password']);
+                $datas->setRole($data["role"]);
+                //changer vraiment
+                session_start();
+                $_SESSION['user'] = $datas;
                 header('location: /accueil');
-
+            }
         }
+        header('location: /accueil');
+
+    }
 
     /**
      * @return void
